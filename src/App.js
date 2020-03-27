6fd50263b4fcfba91eea9a44a48c293b04cd7f0d
yishai <yishai5555@gmail.com>
2020-03-27 16:14:26 +0300

    07

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from './components/button/button';
 
+import Counter from './components/counter/counter';
 import './App.css';
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Button label="click me please" />
+        
+        <Counter/>
+        {/* <Counter/> */}
       </header>
     </div>
   );
