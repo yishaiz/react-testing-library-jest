0871267aabde47465b75843fcccc5c888f3e3ef5
yishai <yishai5555@gmail.com>
2020-03-27 13:11:09 +0300

    05

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 // import "jest-dom/extend-expect"
 
 import Button from '../button';
+
+afterEach(cleanup)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -15,3 +17,9 @@ it('renders button correctly', () => {
   const { getByTestId } = render(<Button label="click me please"></Button>);
   expect(getByTestId('button')).toHaveTextContent('click me please');
 });
+
+it('renders button correctly', () => {
+  const { getByTestId } = render(<Button label="save"></Button>);
+  expect(getByTestId('button')).toHaveTextContent('save');
+});
+
